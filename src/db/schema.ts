c9a54ead63bd8dc742a4d596606c22bce49b52ee
Khariza Viloria <kharizzakaye@gmail.com>
2025-10-20 18:52:42 +0800

    create schema

---
+++ src/db/schema.ts
@@ -0,0 +1,63 @@
+import {
+  pgTable,
+  serial,
+  varchar,
+  boolean,
+  timestamp,
+  integer,
+  text,
+} from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+
+// customers table
+export const customers = pgTable("customers", {
+  id: serial("id").primaryKey(),
+  firstName: varchar("first_name").notNull(),
+  lastName: varchar("first_name").notNull(),
+  email: varchar("email").unique().notNull(),
+  phone: varchar("phone").unique().notNull(),
+  address1: varchar("address1").notNull(),
+  address2: varchar("address2"),
+  city: varchar("city").notNull(),
+  state: varchar("state", { length: 2 }).notNull(),
+  zip: varchar("zip", { length: 10 }).notNull(),
+  noted: text("notes"),
+  active: boolean("active").notNull().default(true),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+});
+
+// tickets table
+export const tickets = pgTable("tickets", {
+  id: serial("id").primaryKey(),
+  customerId: integer("customer_id")
+    .notNull()
+    .references(() => customers.id),
+  title: varchar("title").notNull(),
+  description: text("description"),
+  completed: boolean("completed").notNull().default(false),
+  tech: varchar("tech").notNull().default("unnassigned"),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+});
+
+// create relations
+
+// customers can have many tickets
+export const customerRelations = relations(customers, ({ many }) => ({
+  tickets: many(tickets),
+}));
+
+// a ticket can only have one customer
+export const ticketRelations = relations(tickets, ({ one }) => ({
+  customer: one(customers, {
+    fields: [tickets.customerId],
+    references: [customers.id],
+  }),
+}));
